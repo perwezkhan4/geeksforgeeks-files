1d1331e72715785b2f8916283fc2b3bcc7a2fa0d
Perwez Khan <toperwez@gmail.com>
2020-12-06 23:40:58 +0530

    second

---
+++ Array2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+	  cin>>arr[i];
+  }
+
+
+  int min = INT_MAX;
+  int max = INT_MIN;
+
+  for(int i=0; i<n; i++){
+	  if(arr[i]>max){
+		  max = arr[i];
+	  }
+	  if(arr[i]<min){
+		  min = arr[i];
+	  }
+  }
+
+
+  cout<<"MAX value : "<<max<<endl;
+  cout<<"MIN value : "<<min<<endl;
+
+  return 0;
+}
